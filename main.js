b5f518841576cd1b0436829e5d7b3cebf384a108
angelofaoldi <faoldiangelo@gmail.com>
2024-04-24 08:29:07 +0200

    first step into js

---
+++ main.js
@@ -0,0 +1,27 @@
+// Backtick ` //
+
+'use strict';
+
+// use strict limiterà JS e mi obbligherà a definire le variabili //
+
+    //    Chiedi all’utente il suo nome // 
+    // poi chiedi il suo cognome // 
+    // poi chiedi il suo colore preferito // 
+    // Infine scrivi sulla pagina nomecognomecolorepreferito21 // 
+    
+    // Bonus 1: visualizzate le informazioni anche nella console del browser; //
+    // Bonus 2: provate a generare un numero casuale con Javascript; //
+
+
+    /* 
+    
+    ESEMPIO DI CONCATENAZIONE
+    
+    const nome = "Marco";
+    const cognome = "Rossi";
+    const nomeCompleto = nome + " " + cognome;
+    console.log(nomeCompleto); // Stampa "Marco Rossi"
+
+*/
+
+const username = "name";
